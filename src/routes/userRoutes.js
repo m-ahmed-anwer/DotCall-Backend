57e6710d99a5bfd56e8a20f60b3729df6e201ce3
M.A.Ahmed <ahmedanwer0094@gmail.com>
2024-05-02 06:13:36 +0530

    Test commit to check .env

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -10,10 +10,15 @@ const {
 const router = express.Router();
 
 router.post("/signup", registerUser);
+
 router.post("/login", loginUser);
+
 router.post("/resetPassword", resetPassword);
+
 router.post("/email", checkUserByEmail);
+
 router.post("/passwordChange", checkUserByPhoneNumberToChangePassword);
+
 router.post("/phoneNumber", checkUserByPhoneNumber);
 
 module.exports = router;
