fa477fd2f6c629cd146e36d3a0969cb5a34a51dc
M.A.Ahmed <ahmedanwer0094@gmail.com>
2024-05-31 23:04:03 +0530

    Created Friends Model

--- src/routes/friendsRoute.js
+++ src/routes/friendsRoute.js
@@ -3,6 +3,6 @@ const { addFriends, getFriends } = require("../controllers/friendsController");
 const router = express.Router();
 
 router.post("/addFriends/:userEmail", addFriends);
-router.post("/getFriends/:userEmail", getFriends);
+router.get("/getFriends/:userEmail", getFriends);
 
 module.exports = router;
