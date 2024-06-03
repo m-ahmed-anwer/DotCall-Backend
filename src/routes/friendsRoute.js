44614e017294684c0d57c177e8e4b91362f9baa7
M.A.Ahmed <ahmedanwer0094@gmail.com>
2024-06-03 21:05:31 +0530

    Update changes

--- src/routes/friendsRoute.js
+++ src/routes/friendsRoute.js
@@ -1,5 +1,9 @@
 const express = require("express");
-const { addFriends, getFriends } = require("../controllers/friendsController");
+const {
+  addFriends,
+  getFriends,
+  acceptFriend,
+} = require("../controllers/friendsController");
 const router = express.Router();
 
 router.post("/addFriends/:userEmail", addFriends);
