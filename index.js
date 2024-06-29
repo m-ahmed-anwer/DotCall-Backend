1d0de9ac831791fdb40a16d835823ac7d2731428
M.A.Ahmed <ahmedanwer0094@gmail.com>
2024-06-29 15:52:55 +0530

    Update socket io

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const usersRoutes = require("./src/routes/userRoutes.js");
 const friendsRoutes = require("./src/routes/friendsRoute.js");
-//const callRoute = require("./src/routes/callRoute.js");
+// const callRoute = require("./src/routes/callRoute.js");
 const tokenRoute = require("./src/routes/tokenRoute.js");
 const http = require("http");
 const path = require("path");
@@ -20,64 +20,45 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use("/users", usersRoutes);
 app.use("/friends", friendsRoutes);
-//app.use("/", callRoute);
+// app.use("/", callRoute);
 app.use("/accessToken", tokenRoute);
 app.get("/", (req, res) => {
   res.send("Welcome to DotCall API...");
 });
 
 const server = http.createServer(app);
-server.listen(process.env.PORT || 3000, () => {
-  console.log(`Server is running on port ${process.env.PORT}.`);
+
+// Socket.IO setup
+const { Server } = require("socket.io");
+const io = new Server(server, {
+  cors: {
+    origin: "*", // Adjust this as necessary for your setup
+    methods: ["GET", "POST"],
+  },
 });
 
-// require("dotenv").config();
-// const express = require("express");
-// const bodyParser = require("body-parser");
-// const http = require("http");
-// const { Server } = require("socket.io");
-// const mongoose = require("mongoose");
-// const dotenv = require("dotenv");
+io.on("connection", (socket) => {
+  console.log("a user connected: ", socket.id);
 
-// // Connect to MongoDB
-// mongoose.connect(process.env.MONGODB_URI, {
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true,
-// });
+  socket.on("disconnect", () => {
+    console.log("user disconnected: ", socket.id);
+  });
 
-// // Create an express app
-// const app = express();
+  // Example event for signaling (you can customize this as needed)
+  socket.on("signal", (data) => {
+    console.log("Signal received:", data);
+    // Broadcast the signal to all other clients
+    socket.broadcast.emit("signal", data);
+  });
 
-// // Configure express app
-// app.use(express.json());
-// app.use(bodyParser.urlencoded({ extended: false }));
+  // Handle other custom events
+  socket.on("customEvent", (data) => {
+    console.log("Custom event received:", data);
+    // Emit an event to the client
+    socket.emit("response", { message: "Custom event processed" });
+  });
+});
 
-// // Define routes
-// const usersRoutes = require("./src/routes/userRoutes.js");
-// const callRoute = require("./src/routes/callRoute.js");
-// const tokenRoute = require("./src/routes/tokenRoute.js");
-// app.use("/users", usersRoutes);
-// app.use("/", callRoute);
-// app.use("/accessToken", tokenRoute);
-
-// // Create an HTTP server
-// const server = http.createServer(app);
-
-// // Create a new socket.io instance and attach it to the server
-// const io = new Server(server);
-
-// // Handle incoming WebSocket connections
-// io.on("connection", (socket) => {
-//   console.log("A user connected");
-
-//   // Handle disconnection
-//   socket.on("disconnect", () => {
-//     console.log("A user disconnected");
-//   });
-// });
-
-// // Start the server
-// const PORT = process.env.PORT || 3000;
-// server.listen(PORT, () => {
-//   console.log(`Server is running on port ${PORT}`);
-// });
+server.listen(process.env.PORT || 3000, () => {
+  console.log(`Server is running on port ${process.env.PORT || 3000}.`);
+});
